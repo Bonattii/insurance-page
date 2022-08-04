cbf27e60e846f8ad0a08ca4b60c54c87138f628d
Rodrigo Bonatti <bonattirodrigo123@hotmail.com>
2022-08-03 18:42:03 -0700

    Finish nav from desktop

--- main.js
+++ main.js
@@ -5,6 +5,33 @@ onScroll()
 function onScroll() {
   showNavOnSroll()
   showBackToTopButtonOnScroll()
+  activateMenuAtCurrentSection(home)
+  activateMenuAtCurrentSection(insurances)
+  activateMenuAtCurrentSection(partners)
+  activateMenuAtCurrentSection(about)
+  activateMenuAtCurrentSection(contact)
+}
+
+function activateMenuAtCurrentSection(section) {
+  const targetLine = scrollY + innerHeight / 2
+
+  const sectionTop = section.offsetTop
+  const sectionHeight = section.offsetHeight
+
+  const sectionTopReachOrPassedTargetLine = targetLine >= sectionTop
+  const sectionEndsAt = sectionTop + sectionHeight
+  const sectionEndPassedTargetLine = sectionEndsAt <= targetLine
+
+  const sectionBoudaries =
+    sectionTopReachOrPassedTargetLine && !sectionEndPassedTargetLine
+
+  const sectionId = section.getAttribute('id')
+  const menuElement = document.querySelector(`.menu a[href*=${sectionId}]`)
+
+  menuElement.classList.remove('active')
+  if (sectionBoudaries) {
+    menuElement.classList.add('active')
+  }
 }
 
 function showNavOnSroll() {
